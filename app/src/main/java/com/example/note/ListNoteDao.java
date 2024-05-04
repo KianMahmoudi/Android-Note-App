0284c016d441c5df3d80171696e90225db9a4110
kian mahmoudi <kianmahmoudi9@gmail.com>
2024-05-04 21:09:10 +0330

    add edit opition in listnote

--- app/src/main/java/com/example/note/ListNoteDao.java
+++ app/src/main/java/com/example/note/ListNoteDao.java
@@ -4,6 +4,7 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
@@ -21,5 +22,7 @@ public interface ListNoteDao {
     @Query("SELECT * FROM ListNote WHERE id = :id ")
     ListNote getById(long id);
 
+    @Update
+    void update(ListNote listNote);
 
 }
